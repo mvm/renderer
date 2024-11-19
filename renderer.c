101f658c96de2dd285f3b82d4e1128584b69f724
Miguel Vicente Moure <164623+mvm@users.noreply.github.com>
2024-11-19 14:57:42 +0100

    Create SDL window

--- renderer.c
+++ renderer.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <SDL.h>
 
 int main() {
-    printf("Testing...\n");
+    SDL_Window *window;
+    SDL_Surface *render_surface;
+
+    int err = SDL_Init(SDL_INIT_VIDEO);
+    if(err < 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("Renderer", 0, 0, 1280, 720, 0);
+    if(window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "can't create a window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    render_surface = SDL_GetWindowSurface(window);
+
+    Uint64 start_time = SDL_GetTicks64();
+    unsigned int quit = 0;
+
+    do {
+        Uint64 now = SDL_GetTicks64();
+
+        if((now - start_time > 5*1000)) break;
+        SDL_UpdateWindowSurface(window);
+    } while(1);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }

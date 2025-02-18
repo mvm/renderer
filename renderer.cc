d0b21f6f8b53683d653e04b72396ead8c5311b53
Miguel Vicente Moure <164623+mvm@users.noreply.github.com>
2025-02-18 10:39:42 +0100

    Fixed drawing line, added more tests

--- renderer.cc
+++ renderer.cc
@@ -17,43 +17,55 @@ void handle_events(SDL_Event *event) {
         quit = true;
 }
 
-void draw_line(SDL_Renderer *renderer, Uint32 color, int x1, int y1, int x2, int y2) {
+void draw_line(SDL_Renderer *renderer, Uint32 color, int _x1, int _y1, int _x2, int _y2) {
     float slope;
     bool flip_axis = false;
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
 
+    int x1, x2, y1, y2;
+    x1 = _x1;
+    x2 = _x2;
+    y1 = _y1;
+    y2 = _y2;
+
     if(x2 == x1) {
         flip_axis = true;
     } else {
         float tmpslope = (y2 - y1) / (float)(x2 - x1);
-        if(tmpslope > 0.5) {
+        if(abs(tmpslope) > 1) {
             flip_axis = true;
         }
     }
 
     if(flip_axis == false) {
+        if(x1 > x2) {
+            x1 = _x2;
+            y1 = _y2;
+
+            x2 = _x1;
+            y2 = _y1;
+        }
         slope = (y2 - y1) / (float)(x2 - x1);
     } else {
+        if(y1 > y2) {
+            y1 = _y2;
+            x1 = _x2;
+
+            y2 = _y1;
+            x2 = _x1;
+        }
         slope = (x2 - x1) / (float)(y2 - y1);
     }
     
     if(flip_axis) {
-        if(y1 > y2) {
-            int t = y2;
-            y2 = y1;
-            y1 = t;
-        }
+        SDL_assert(y1 < y2);
         for(int y = y1; y < y2; y++) {
             int x = (int)SDL_floorf(slope*y) + x1;
             SDL_RenderDrawPoint(renderer, x, y);
         }
     } else {
-        if(x1 > x2) {
-            int t = x2;
-            x2 = x1;
-            x1 = t;
-        }
+        SDL_assert(x1 < x2);
         for(int x = x1; x < x2; x++) {
             int y = (int)SDL_floorf(slope*x) + y1;
             SDL_RenderDrawPoint(renderer, x, y);
@@ -97,10 +109,13 @@ int main()
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        draw_line(renderer, -1, 0, 0, WIDTH, HEIGHT);
-        draw_line(renderer, -1, 0, HEIGHT, WIDTH, 0);
-        draw_line(renderer, -1, WIDTH/2, 0, WIDTH/2, HEIGHT);
-        draw_line(renderer, -1, 0, HEIGHT/2, WIDTH, HEIGHT/2);
+        draw_line(renderer, -1, WIDTH/2 + 30, 0, WIDTH/2 - 30, HEIGHT);
+        draw_line(renderer, -1, WIDTH/2 - 30, 0, WIDTH/2 + 30, HEIGHT);
+        
+        draw_line(renderer, -1, 0, HEIGHT/2 - 30, WIDTH, HEIGHT/2 + 30);
+        draw_line(renderer, -1, WIDTH, HEIGHT/2 - 30, 0, HEIGHT/2 + 30);
+        
+        draw_line(renderer, -1, WIDTH/2, -50, WIDTH/2, HEIGHT + 50);
 
         SDL_RenderPresent(renderer);
     }
